b5d6c00f674e446d41c05199f3f562588a4fea4c
ylloyd <yvan@oxynum.fr>
2016-04-05 19:09:06 +0200

    Changed schema

--- models/restaurants.js
+++ models/restaurants.js
@@ -1,7 +1,26 @@
 const mongoose = require('mongoose');
 
 const RestaurantsSchema = new mongoose.Schema({
-  // Complete schema...
+  address: {
+     building: { type: String, default: '' },
+     coord: [
+     	{ type: Double, default: ''},
+     	{ type: Double, default: '' }
+     ],
+     street: { type: String, default: '' },
+     zipcode: { type: String, default: '' }
+  },
+  borough: { type: String, default: '' },
+  cuisine: { type: String, default: '' },
+  grades: [
+     {
+     	date: { type: String, default: '' },
+     	grade: { type: String, default: '' },
+     	score: { type: INT, default: '' }
+     }
+  ],
+  name: { type: String, default: '' },
+  restaurant_id: { type: String, default: '' }
 });
 
 module.exports = mongoose.model('Restaurant', RestaurantsSchema);
--- routes/restaurants.js
+++ routes/restaurants.js
@@ -2,12 +2,19 @@ var express = require('express');
 var router = express.Router();
 var Restaurant = require('../models/restaurants');
 
+const mongoose = require('mongoose');
+const Restaurants = mongoose.model('Restaurant');
+
 // Model available :
 // eg. Restaurant.find({...}).exec().then(...)
 
 /* GET restaurants listing. */
 router.get('/', function(req, res, next) {
-  res.render('restaurants/index', {});
+	Restaurants.find().exec().then((res) => {
+		console.log(res);
+		res.render('restaurants/index', {});
+	});
+		
 });
 
 module.exports = router;
