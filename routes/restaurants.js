c9e2ee89988df29ca067abf7635dc3641fc3558b
Vincent TAING <vnctaing@vnctaingggg.local>
2016-04-10 14:03:03 +0200

    WIP view restaurant

--- routes/restaurants.js
+++ routes/restaurants.js
@@ -10,4 +10,17 @@ router.get('/', function(req, res, next) {
   res.render('restaurants/index', {});
 });
 
+router.get('/view/:id', (req, res, next) => {
+    console.log(req.params.id)
+    Restaurant.findById(req.params.id, function (err, restaurant) {
+      if (err) console.log(err);
+      if (restaurant) {
+        res.render('restaurants/view',restaurant)
+      } else{
+        res.json({status:404, 'message': 'Did not found user with that password'});
+      }
+    })
+
+})
+
 module.exports = router;
