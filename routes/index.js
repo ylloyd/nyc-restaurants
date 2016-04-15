cc63b6b5538183365e8039503d2e9f30bf165ecc
ylloyd <yvan@oxynum.fr>
2016-04-15 22:52:43 +0200

    Display restaurants and comments

--- routes/index.js
+++ routes/index.js
@@ -1,31 +1,36 @@
-const express = require('express');
-const router = express.Router();
-const Restaurant = require('../models/restaurants');
-const Comment = require('../models/comment');
+"use strict";
+
+var express = require('express');
+var router = express.Router();
+var Restaurant = require('../models/restaurants');
+var Comment = require('../models/comment');
 
 /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   Restaurant.find({restaurant_id:'30112340'}).then((restaurant) => {
+//     console.log(restaurant);
+//     res.render('restaurants/restaurant', {restaurant});
+//   }), ((err) => {
+//     console.log(err)
+//   });
+//   res.render('index', { title: 'Express' });
+// });
+
+const getComments = (() => {
+	return Comment.find({}).sort('-createdDate').limit(10).exec();
+});
+
 router.get('/', function(req, res, next) {
-    Comment
-        .find({})
-        .sort('-createdDate')
-        .limit(10)
-        .exec((err,comments) => res.render('index',{comments}))
+    let comments;
+    let resto;
 
-    Restaurant
-        .findOne({}, 'grades', function(err, restaurants) {
-            
-            Restaurant.aggregate([
-                { $match: { _id: restaurants._id } },
-                {
-                    $group: {
-                        _id: '$_id', 
-                        average: {$avg: '$grades.score'}
-                    }
-                }
-            ], function (err, result) {console.log(result)});
-        });
-
-  
+    Restaurant.aggregate([{$unwind:'$grades'}, {$match:{'grades.score': { $ne : 0 } }}, {$group:{_id: {id:'$_id', name:'$name', cuisine:'$cuisine', address:'$address.building'}, average: {$avg: '$grades.score'}}}]).sort('-average').limit(10).exec().then((restaurants) => {
+    	resto = restaurants;
+    	return getComments();
+    }).then((comments) => {
+      	//console.log(resto);
+      	res.render('index', {restaurants: resto, comments});
+    });
 });
 
 module.exports = router;
