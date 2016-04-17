75124f7ea3801dcae5f0ddbe1bb3f6a9fa1170fc
Vincent TAING <vnctaing@vnctaingggg.local>
2016-04-17 20:30:27 +0200

    Render the top 10 average restaurants

--- routes/index.js
+++ routes/index.js
@@ -5,25 +5,53 @@ const Comment = require('../models/comment');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    Comment
-        .find({})
-        .sort('-createdDate')
-        .limit(10)
-        .exec((err,comments) => res.render('index',{comments}))
+    const p1 = Comment
+                    .find({})
+                    .sort('-createdDate')
+                    .limit(10)
+                    // .exec((err,comments) => res.render('index',{comments}))
 
-    Restaurant
-        .findOne({}, 'grades', function(err, restaurants) {
-            
-            Restaurant.aggregate([
-                { $match: { _id: restaurants._id } },
-                {
-                    $group: {
-                        _id: '$_id', 
-                        average: {$avg: '$grades.score'}
-                    }
-                }
-            ], function (err, result) {console.log(result)});
+    const p2 = Restaurant
+                    .aggregate([
+                        {$unwind:'$grades'},
+                        {$match:{'grades.score': { $ne : 0 } }}, 
+                        {$group:
+                            {
+                                _id: {
+                                    id:'$_id', 
+                                    name:'$name',
+                                    cuisine:'$cuisine', 
+                                }, 
+                                average: {$avg: '$grades.score'}
+                            }
+                        }
+                    ])
+                    .sort('-average')
+                    .limit(10)
+                    .exec()
+
+    Promise
+        .all([p1,p2])
+        .then((data) => {
+            res.render('index', {
+                comments: data[0],
+                bestRestaurants: data[1]
+            });
         });
+
+    // Restaurant
+    //     .findOne({}, 'grades', function(err, restaurants) {
+            
+    //         Restaurant.aggregate([
+    //             { $match: { _id: restaurants._id } },
+    //             {
+    //                 $group: {
+    //                     _id: '$_id', 
+    //                     average: {$avg: '$grades.score'}
+    //                 }
+    //             }
+    //         ], function (err, result) {console.log(result)});
+    //     });
 
   
 });
